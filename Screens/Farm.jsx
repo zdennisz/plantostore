574ec07fd6ad7d96e57b9f0fc018b4e0b1c30219
Dennis Z <zdennisz@gmail.com>
2021-09-05 13:03:16 +0300

    added two header buttons

--- Screens/Farm.jsx
+++ Screens/Farm.jsx
@@ -1,18 +1,37 @@
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet,View,Text, Platform } from "react-native";
 import React, { useEffect } from 'react'
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/CustomHeaderButtons";
 
 
 const Farm=(props)=>{
     const {route,navigation}=props
     
-
+    const goToCart=()=>{
+        navigation.navigate('cart')
+    }
 
 
     useEffect(()=>{
         navigation.setOptions({
-            title:`Farm ${route.params.farm}`
-        })
-    })
+            title:`Farm ${route.params.farm}`,
+            headerRight:()=>(
+                <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                    <Item 
+                    title='Cart'
+                    iconName={Platform.OS ==='android'?'md-cart':'ios-cart'}
+                    onPress={goToCart}/>
+                    <Item
+                      title='Store'
+                      iconName={Platform.OS ==='android'?'md-add-circle-sharp':'ios-add-sharp'}
+                      onPress={goToCart}/>
+                  
+                </HeaderButtons>
+            )
+            
+        });
+    },[]);
+    
     return (
             <View></View>
     )
---
+++ components/CustomHeaderButtons.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Platform } from "react-native";
+import { HeaderButton } from "react-navigation-header-buttons";
+import { Ionicons } from "@expo/vector-icons";
+
+
+const CustomHeaderButton = (props) => {
+	return (
+		<HeaderButton
+			{...props}
+			IconComponent={Ionicons}
+			iconSize={23}
+		></HeaderButton>
+	);
+};
+
+export default CustomHeaderButton;
