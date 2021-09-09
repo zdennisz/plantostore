77099b075e501212f97288ea51f4625047ea330e
Dennis Z <zdennisz@gmail.com>
2021-09-09 23:47:36 +0300

    fixed ui round buttons

--- components/CustomRoundButton.jsx
+++ components/CustomRoundButton.jsx
@@ -7,14 +7,11 @@ const CustomButton = (props) => {
 	const onPressHandler = () => {
 		props.pressHandler();
 	};
-	//TODO merge the custom round button with custom button
+
 	return (
-		<View>
-			<TouchableOpacity style={styles.container} onPress={onPressHandler}>
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.btnContainer} onPress={onPressHandler}>
 				<View style={styles.contentContainer}>
-					{props.isImage ? (
-						<Ionicons name='leaf' size={35} style={styles.image} />
-					) : null}
 					<Text style={styles.text}>{props.title}</Text>
 				</View>
 			</TouchableOpacity>
@@ -25,18 +22,23 @@ const CustomButton = (props) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btnContainer: {
+		flex: 1,
 		flexDirection: "row",
 		justifyContent: "center",
 		alignItems: "center",
-		height: 36,
-		width: 36,
+		maxHeight: 36,
+		maxWidth: 36,
 		backgroundColor: Colors.accent,
 		borderRadius: 25,
 		elevation: 5,
 	},
 	text: {
 		color: "white",
-		fontSize: 24,
+		fontSize: 28,
 	},
 	image: {
 		color: "white",
