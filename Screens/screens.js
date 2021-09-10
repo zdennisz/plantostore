4f4a00443cf6cb3300b1339882d225a99bf73b4e
Dennis Z <zdennisz@gmail.com>
2021-09-10 22:40:29 +0300

    reafactored files location

---
+++ Screens/screens.js
@@ -0,0 +1,10 @@
+import LogIn from './LogIn';
+import Farms from './Farms'
+import Farm from './Farm'
+import Store from './Store'
+import Cart from './Cart'
+import VeggieDesc from './VeggieDesc'
+
+
+const Screens = {}
+export default Screens
